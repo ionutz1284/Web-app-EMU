03a225fa4a503dfd4f787f7020293b9472d4f553
ionutz1284 <ionutz1284@gmail.com>
2017-03-22 11:31:28 -0400

    sdfgsdfgsdfgs sdfg

---
+++ ContactManager/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome to Eastern Michigan University website";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Let's keep in touch";
+
+            return View();
+        }
+    }
+}
